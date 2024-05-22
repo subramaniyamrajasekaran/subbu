ee6959cc9376ef5225d1d6a7e785f18d3725c9e6
subramaniyamrajasekaran <bharathi07101998@gmail.com>
2024-05-22 11:58:27 +0530

    i m done

---
+++ src/sring/tyoeS.java
@@ -0,0 +1,10 @@
+package sring;
+
+public class tyoeS {
+public static void main(String[] args) {
+	String s="Automation Testing";
+	int length = s.length();
+	System.out.println("Length of the word: "+length);
+	
+}
+}
